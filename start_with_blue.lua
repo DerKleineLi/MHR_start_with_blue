7c1ce43542be5bf5c87a28c3941a6de1601e39ae
DerKleineLi <lhx393264301@163.com>
2022-08-31 17:28:07 +0200

    clean up

--- start_with_blue.lua
+++ start_with_blue.lua
@@ -35,11 +35,11 @@ function IsModuleAvailable(name)
         return true
     else
         for _, searcher in ipairs(package.searchers or package.loaders) do
-        local loader = searcher(name)
-        if type(loader) == 'function' then
-            package.preload[name] = loader
-            return true
-        end
+            local loader = searcher(name)
+            if type(loader) == 'function' then
+                package.preload[name] = loader
+                return true
+            end
         end
         return false
     end
